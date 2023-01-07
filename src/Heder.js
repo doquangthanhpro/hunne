fe10e90f85806e2ad49c9696554fc2da375756ba
hung <trantronghung9112003.com>
2023-01-07 16:49:32 +0700

    first commit

---
+++ src/Heder.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Heder() {
+  return (
+    <div><link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+    <div className="dau">
+      <div className="img">
+        {/* <img src='https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/eaf6e5daa8a7286785abb28fc52ee801~c5_100x100.jpeg?x-expires=1673053200&x-signature=5TZUGo41zKdmsg91rWe0oC%2F9%2F0M%3D'></img> */}
+      </div>
+      <div className='text1'>
+          <text>
+              <h1> Trần Trọng Hùng </h1>
+              <h4>Lập Trình Viên Java</h4>     
+          </text>
+      </div>
+
+    </div></div>
+  )
+}
+
+export default Heder
