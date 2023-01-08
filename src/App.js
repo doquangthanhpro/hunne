b61462455c62d8710a68372ca992244b5bcb09fb
quang thanh <minhhacong912@gmail.com>
2023-01-08 18:03:42 +0700

    kiad

--- src/App.js
+++ src/App.js
@@ -1,121 +1,145 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
- import { useState } from 'react';
- const dataSkill = [
-  {
-    name : 'NodeJs', 
-     typeOne: 'a11',
-      typeTwo : 'b11'
-  } ,
-     {
-    name : 'VueJs', 
-     typeOne: 'a12',
-      typeTwo : 'b12'
-  } ,
- {
-    name : 'PHP', 
-     typeOne: 'a13',
-      typeTwo : 'b13'
-  } ,
-  {
-    name : 'Tiếng Anh', 
-     typeOne: 'a14',
-      typeTwo : 'b14'
-  } ,
-  
-]
- const DataCertificate = [ 
-    {
-      title : ' PHP , JAVASCRIPT , MYSQL' },
-    {
-      title : ' LARAVEL FRAMEWORK , JQUERY , REACT' },
-    {
-      title : ' GIT , LINUX , REDIS, MONGOB' },
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./Header";
 
-    ] 
+const DataCertificate = [
+  {
+    title: " PHP , JAVASCRIPT , MYSQL ,FULLSTACK",
+  },
+  {
+    title: " LARAVEL FRAMEWORK , JQUERY , REACT",
+  },
+  {
+    title: " GIT , LINUX , REDIS, MONGOB",
+  },
+];
 
 function App() {
-   const [skill , setSkill] = useState(dataSkill || [])
-   const [chungchi , setchungchi] = useState(DataCertificate || [])
+  const [Input, setInput] = useState(300);
+  const dataSkill = [
+    {
+      name: "NodeJs",
+      typeOne: "a11",
+      input: 30,
+      id: 1,
+    },
+    {
+      name: "VueJs",
+      typeOne: "a12",
+      input: 50,
+      id: 2,
+    },
+    {
+      name: "PHP",
+      typeOne: "a13",
+      input: 300,
+      id: 3,
+    },
+    {
+      name: "Tiếng Anh",
+      typeOne: "a14",
+      input: 189,
+      id: 4,
+    },
+  ];
+  const [skill, setSkill] = useState(dataSkill || []);
+  const [chungchi, setchungchi] = useState(DataCertificate || []);
+
   return (
-    <div className='all'>
-      {/* props */}
- <Header author={'Trần Trọng Hùng '} address={'259 Hà Huy tập'} email={'trantronghung@gmail.com'} phoneNumber={'03993784848'} job={'Lập trình viên Java'}/>
+    <div className="all">
+      <Header
+        author={"Trần Trọng Hùng "}
+        address={"259 Hà Huy tập"}
+        email={"trantronghung@gmail.com"}
+        phoneNumber={"03993784848"}
+        job={"Lập trình viên Java"}
+      />
+      {/* form model */}
+      {/* view element */}
+      <div className="thongtin">
+        <div className="tt1">
+          <h2>MỤC TIÊU NGHỀ NGHIỆP</h2>
+          <p>
+            Mong muốn trở thành một PM lead team 10 nhân sự để nâng cấp các sản
+            phẩm chạy nhanh và ổn định hơn cho công ty . Ngoài ra , tôi còn mong
+            muốn được làm trong một môi trường chuyên nghiệp , đồng nghiệp hòa
+            đổng phúc lợi tốt
+          </p>
+        </div>
 
-     <div className='thongtin'>
-         <div className='tt1'>
-            <h2>MỤC TIÊU NGHỀ NGHIỆP</h2>       
-            <p>Mong muốn trở thành một PM lead team 10 nhân sự để nâng cấp các sản phẩm chạy nhanh và ổn định hơn cho công ty .
-              Ngoài ra , tôi còn mong muốn được làm trong một môi trường chuyên nghiệp , đồng nghiệp hòa đổng phúc lợi tốt  </p>     
-          </div>
-          <div className='kynang'>
-            <h2>Kỹ Năng     </h2>
-             {
-              skill.map((e, i) => {
-                 return (
-                   <h5 key={i}> {e.name} <a href='#/' className={e.typeOne}> </a> <a href='#/' className={e.typeTwo}> </a> </h5>
-                 ) 
-              })
-             } 
-          </div>
-      
-          <div className='cc'>
-              <h2>Chứng Chỉ </h2>
-            {
-              chungchi.map((e, i) => {
-                 return (
-                  <h5 key={i}>{e.title}</h5>
-                 )
-              })
-            }  
-          </div>
-          <div className='hv'>
-              <h2>HỌC VẤN </h2>
-              <h3>Chuyên Ngành Công Nghệ Tông Tin </h3>
-              <h4>Cao Đẳng FPT poly</h4>
-              <h5>Tốt NGhiệp Loại Giỏi, Điểm Trung Bình 8.0</h5>
-          </div>
+        <div className="kynang">
+          <h2>Kỹ Năng</h2>
+          <button
+          className="absolute mt-[180px] ml-[530px] bg-red-700 px-4 "
+            onClick={() => {
+              setInput((e) => e + 5);
+            }}
+          >
+            add
+          </button>
+        
 
-          <div className='knlv'>
-              <h2>Kinh Nghiệm Làm Việc </h2>
-              <h3>SENIOR DEVELOPER </h3>
-              <h4>Công Ty Công Nghệ B</h4>
-              <h5>Thực hiện các dự án PHP</h5>
-          </div>
-
-          <div className='hd'>
-              <h2>Hoạt Động  </h2>
-              <h3>Thành Viên </h3>
-              <h4>Tham Gia Học Tại Trường FPT POLY</h4>
-              <h5>Cùng Team Đạt Giải</h5>
-          </div>
-
-          <div className='ctcn'>
-              <h2>JUNIOR DEVELOPER  </h2>
-              <h3>Công Ty Công Nghệ B</h3>             
-              <h5>Thực hiện các dự án big data </h5>
-          </div>
-
-          <div className='gt'>
-              <h2>Giải Thưởng   </h2>
-              <h3>Nhân Viên Xuất Sắc Nhắt Công Ty FPOLY </h3>
-              
-          </div>
-
-          <div className='st'>
-              <h2>Sở Thích    </h2>
-              <ul>
-                <li> Chơi Game </li>
-                <li> Đi Chơi  </li>
-                <li> Xem Phim  </li>              
-              </ul>
-              
-          </div>
-
-
-
-      </div>    
+          {skill.map((e, i) => {
+            return (
+              <h5 key={i}>
+                {e.name}
+                <a
+                  href="#/"
+                  Style={`width: ${e.input}px`}
+                  className={e.typeOne}
+                ></a>
+                <a href="#/" className="b11"></a>
+              </h5>
+            );
+          })}
+           <h5 className="">
+            {'fullStack'}
+            <a href="#/" Style={`width: ${Input}px`} className={"a17"}></a>
+            <a href="#/" className="b11"></a>
+          </h5>
+        </div>
+        <div className="cc">
+          <h2>Chứng Chỉ </h2>
+          {chungchi.map((e, i) => {
+            return <h5 key={i}>{e.title}</h5>;
+          })}
+        </div>
+        <div className="hv">
+          <h2>HỌC VẤN </h2>
+          <h3>Chuyên Ngành Công Nghệ Tông Tin </h3>
+          <h4>Cao Đẳng FPT poly</h4>
+          <h5>Tốt NGhiệp Loại Giỏi, Điểm Trung Bình 8.0</h5>
+        </div>
+        <div className="knlv">
+          <h2>Kinh Nghiệm Làm Việc </h2>
+          <h3>SENIOR DEVELOPER </h3>
+          <h4>Công Ty Công Nghệ B</h4>
+          <h5>Thực hiện các dự án PHP</h5>
+        </div>
+        <div className="hd">
+          <h2>Hoạt Động </h2>
+          <h3>Thành Viên </h3>
+          <h4>Tham Gia Học Tại Trường FPT POLY</h4>
+          <h5>Cùng Team Đạt Giải</h5>
+        </div>
+        <div className="ctcn">
+          <h2>JUNIOR DEVELOPER </h2>
+          <h3>Công Ty Công Nghệ B</h3>
+          <h5>Thực hiện các dự án big data </h5>
+        </div>
+        <div className="gt">
+          <h2>Giải Thưởng </h2>
+          <h3>Nhân Viên Xuất Sắc Nhắt Công Ty FPOLY </h3>
+        </div>
+        <div className="st">
+          <h2>Sở Thích </h2>
+          <ul>
+            <li> Chơi Game </li>
+            <li> Đi Chơi </li>
+            <li> Xem Phim </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
-function Header({author , orther , email , phoneNumber , address}) {
+function Header({author , job , email , phoneNumber , address}) {
   return (
     <div>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-    <div className="dau">
+    <div className="dau text-center">
       <div className="img">
          <img src='https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/eaf6e5daa8a7286785abb28fc52ee801~c5_100x100.jpeg?x-expires=1673053200&x-signature=5TZUGo41zKdmsg91rWe0oC%2F9%2F0M%3D' alt='er'/> 
       </div>
       <div className='text1'>
-          <text>
-              <h1>{author}</h1>
-              <h4>{orther}</h4>     
+          <text className='  text-center font-bold  '>
+              <h1 className='text-center font-bold'>{author}</h1>
+              <h4 className=' text-gray-400 rounded-lg font-bold'>{job}</h4>     
           </text>
       </div>
 
@@ -19,9 +19,7 @@ function Header({author , orther , email , phoneNumber , address}) {
      <div className='info'>
           <i className="fa fa-cloud"></i>
           <a href="#/">{email}</a>
-
           <i className="fa fa-heart"></i>
-
           <a href="#/">{phoneNumber}</a>
           <i className="fa fa-car"></i>
           <a href="#/">{address}</a>
